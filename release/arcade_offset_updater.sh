966bf02a999dfbbe4bd34f54d880312786b3d33e
Frédéric Mahé <frederic.mahe@cirad.fr>
2021-07-06 14:14:08 +0200

    code refactoring
    - don't repeat yourself,
    - use line breaks to make sure that `operationA && operationB && operationC` are properly queued,
    - use long option names (carry more intent than short option names)
    
    This is my OCD talking, sorry about that :-)

--- release/arcade_offset_updater.sh
+++ release/arcade_offset_updater.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#written by atrac17
+# written by atrac17
 
 RED='\033[0;31m'
 PRP='\033[0;35m'
 NC='\033[0m'
+FOLDER="/media/fat/_Arcade/_Arcade Offset"
+CERT_URL="/etc/ssl/certs/cacert.pem"
+ARCHIVE_URL="https://raw.githubusercontent.com/atrac17/Arcade_Offset/main/release/misterfpga_arcade_offset-20210706.txz"
 
-echo -e ${NC}
-echo -e "${PRP}Arcade Offset ${NC}is updating..."
-echo 
-echo -e -n "Grabbing latest ${PRP}Arcade Offset ${NC}MRA files..."
-echo 
-rm -rf "/media/fat/_Arcade/_Arcade Offset" &&
-mkdir -p "/media/fat/_Arcade/_Arcade Offset" &&
-cd "/media/fat/_Arcade/_Arcade Offset" &&
-curl -s --cacert /etc/ssl/certs/cacert.pem https://raw.githubusercontent.com/atrac17/Arcade_Offset/main/release/misterfpga_arcade_offset-20210706.txz | tar -Jxf - --no-same-owner --no-same-permissions &&
-echo 
-echo -e "${PRP}Update Complete"
-echo -e ${NC}
+echo -e "${NC}"
+echo -e "${PRP}Arcade Offset${NC} is updating..."
+echo
+echo -e "Grabbing latest ${PRP}Arcade Offset${NC} MRA files..."
+
+rm --recursive --force "${FOLDER}" && \
+    mkdir --parents "${FOLDER}" && \
+    cd "${FOLDER}" && \
+    curl --silent --cacert "${CERT_URL}" "${ARCHIVE_URL}" | \
+        tar --extract --xz --no-same-owner --no-same-permissions --file=- && \
+    echo -e "\n${PRP}Update Complete${NC}"
 
 exit 0
